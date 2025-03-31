6e52331d0dcb05e24195f7a5ec62d96f06a0acf0
Gergish <g.berdnikov674@gmail.com>
2025-03-31 11:29:23 +0300

    Debugvar means nothing

--- Amalgam/src/SDK/Vars.h
+++ Amalgam/src/SDK/Vars.h
@@ -89,7 +89,7 @@ inline ConfigVar<T>::ConfigVar(T value, std::string name, int iFlags)
 #define VISUAL (1 << 0)
 #define NOSAVE (1 << 1)
 #define NOBIND (1 << 2)
-#define DEBUGVAR NOSAVE
+#define DEBUGVAR NONE
 
 namespace Vars
 {
